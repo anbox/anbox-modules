0338a3497961eaaa7bc01b74c611208429385ea9
Christian Hoff <christian_hoff@gmx.net>
2021-09-26 18:54:51 +0200

    Update binder to the latest version
    Over the last years, many fixes and changes have been applied to
    Binder in the Linux kernel, but these fixes were never backported
    to anbox-modules. The meant that the version of Binder in anbox-modules
    was very outdated.
    
    With this commit, Binder has been update to the latest state from
    the Linux kernel. This should also fix the compile on kernel 5.12
    and later.

--- binder/deps.c
+++ binder/deps.c
@@ -2,12 +2,15 @@
 #include <linux/file.h>
 #include <linux/fdtable.h>
 #include <linux/atomic.h>
+#include <linux/ipc_namespace.h>
 #include <linux/mm.h>
 #include <linux/slab.h>
 #include <linux/spinlock.h>
 #include <linux/kallsyms.h>
 #include <linux/kprobes.h>
 #include <linux/version.h>
+
+#include "deps.h"
 
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(5,7,0))
 
@@ -66,103 +69,16 @@ static unsigned long kallsyms_lookup_name_wrapper(const char *name)
 }
 
 
-static struct vm_struct *(*get_vm_area_ptr)(unsigned long, unsigned long) = NULL;
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 11, 0)
-static void (*zap_page_range_ptr)(struct vm_area_struct *, unsigned long, unsigned long) = NULL;
-#else
-static void (*zap_page_range_ptr)(struct vm_area_struct *, unsigned long, unsigned long, struct zap_details *) = NULL;
-#endif
-static int (*map_kernel_range_noflush_ptr)(unsigned long start, unsigned long size, pgprot_t prot, struct page **pages) = NULL;
-static void (*unmap_kernel_range_ptr)(unsigned long, unsigned long) = NULL;
-static struct files_struct *(*get_files_struct_ptr)(struct task_struct *) = NULL;
-static void (*put_files_struct_ptr)(struct files_struct *) = NULL;
-static struct sighand_struct *(*__lock_task_sighand_ptr)(struct task_struct *, unsigned long *) = NULL;
-static int (*__alloc_fd_ptr)(struct files_struct *files, unsigned start, unsigned end, unsigned flags) = NULL;
-static void (*__fd_install_ptr)(struct files_struct *files, unsigned int fd, struct file *file) = NULL;
-static int (*__close_fd_ptr)(struct files_struct *files, unsigned int fd) = NULL;
+static int (*__close_fd_get_file_ptr)(unsigned int fd, struct file **res) = NULL;
+
+int __close_fd_get_file(unsigned int fd, struct file **res)
+{
+    if (!__close_fd_get_file_ptr)
+		__close_fd_get_file_ptr = kallsyms_lookup_name_wrapper("__close_fd_get_file");
+    return __close_fd_get_file_ptr(fd, res);
+}
+
 static int (*can_nice_ptr)(const struct task_struct *, const int) = NULL;
-static int (*security_binder_set_context_mgr_ptr)(struct task_struct *mgr) = NULL;
-static int (*security_binder_transaction_ptr)(struct task_struct *from, struct task_struct *to) = NULL;
-static int (*security_binder_transfer_binder_ptr)(struct task_struct *from, struct task_struct *to) = NULL;
-static int (*security_binder_transfer_file_ptr)(struct task_struct *from, struct task_struct *to, struct file *file) = NULL;
-
-struct vm_struct *get_vm_area(unsigned long size, unsigned long flags)
-{
-	if (!get_vm_area_ptr)
-		get_vm_area_ptr = kallsyms_lookup_name_wrapper("get_vm_area");
-	return get_vm_area_ptr(size, flags);
-}
-
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 11, 0)
-void zap_page_range(struct vm_area_struct *vma, unsigned long address, unsigned long size)
-#else
-void zap_page_range(struct vm_area_struct *vma, unsigned long address, unsigned long size, struct zap_details *details)
-#endif
-{
-	if (!zap_page_range_ptr)
-		zap_page_range_ptr = kallsyms_lookup_name_wrapper("zap_page_range");
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 11, 0)
-	zap_page_range_ptr(vma, address, size);
-#else
-	zap_page_range_ptr(vma, address, size, details);
-#endif
-}
-
-int map_kernel_range_noflush(unsigned long start, unsigned long size, pgprot_t prot, struct page **pages)
-{
-	if (!map_kernel_range_noflush_ptr)
-		map_kernel_range_noflush_ptr = kallsyms_lookup_name_wrapper("map_kernel_range_noflush");
-	return map_kernel_range_noflush_ptr(start, size, prot, pages);
-}
-
-void unmap_kernel_range(unsigned long addr, unsigned long size)
-{
-	if (!unmap_kernel_range_ptr)
-		unmap_kernel_range_ptr = kallsyms_lookup_name_wrapper("unmap_kernel_range");
-	unmap_kernel_range_ptr(addr, size);
-}
-
-struct files_struct *get_files_struct(struct task_struct *task)
-{
-	if (!get_files_struct_ptr)
-		get_files_struct_ptr = kallsyms_lookup_name_wrapper("get_files_struct");
-	return get_files_struct_ptr(task);
-}
-
-void put_files_struct(struct files_struct *files)
-{
-	if (!put_files_struct_ptr)
-		put_files_struct_ptr = kallsyms_lookup_name_wrapper("put_files_struct");
-	put_files_struct_ptr(files);
-}
-
-struct sighand_struct *__lock_task_sighand(struct task_struct *tsk, unsigned long *flags)
-{
-	if (!__lock_task_sighand_ptr)
-		__lock_task_sighand_ptr = kallsyms_lookup_name_wrapper("__lock_task_sighand");
-	return __lock_task_sighand_ptr(tsk, flags);
-}
-
-int __alloc_fd(struct files_struct *files, unsigned start, unsigned end, unsigned flags)
-{
-	if (!__alloc_fd_ptr)
-		__alloc_fd_ptr = kallsyms_lookup_name_wrapper("__alloc_fd");
-	return __alloc_fd_ptr(files, start, end, flags);
-}
-
-void __fd_install(struct files_struct *files, unsigned int fd, struct file *file)
-{
-	if (!__fd_install_ptr)
-		__fd_install_ptr = kallsyms_lookup_name_wrapper("__fd_install");
-	__fd_install_ptr(files, fd, file);
-}
-
-int __close_fd(struct files_struct *files, unsigned int fd)
-{
-	if (!__close_fd_ptr)
-		__close_fd_ptr = kallsyms_lookup_name_wrapper("__close_fd_ptr");
-	return __close_fd_ptr(files, fd);
-}
 
 int can_nice(const struct task_struct *p, const int nice)
 {
@@ -171,12 +87,25 @@ int can_nice(const struct task_struct *p, const int nice)
 	return can_nice_ptr(p, nice);
 }
 
+static void (*mmput_async_ptr)(struct mm_struct *mm) = NULL;
+
+void mmput_async(struct mm_struct *mm)
+{
+	if (!mmput_async_ptr)
+		mmput_async_ptr = kallsyms_lookup_name_wrapper("mmput_async");
+	return mmput_async_ptr(mm);
+}
+
+static int (*security_binder_set_context_mgr_ptr)(struct task_struct *mgr) = NULL;
+
 int security_binder_set_context_mgr(struct task_struct *mgr)
 {
 	if (!security_binder_set_context_mgr_ptr)
 		security_binder_set_context_mgr_ptr = kallsyms_lookup_name_wrapper("security_binder_set_context_mgr");
 	return security_binder_set_context_mgr_ptr(mgr);
 }
+
+static int (*security_binder_transaction_ptr)(struct task_struct *from, struct task_struct *to) = NULL;
 
 int security_binder_transaction(struct task_struct *from, struct task_struct *to)
 {
@@ -185,6 +114,8 @@ int security_binder_transaction(struct task_struct *from, struct task_struct *to
 	return security_binder_transaction_ptr(from, to);
 }
 
+static int (*security_binder_transfer_binder_ptr)(struct task_struct *from, struct task_struct *to) = NULL;
+
 int security_binder_transfer_binder(struct task_struct *from, struct task_struct *to)
 {
 	if (!security_binder_transfer_binder_ptr)
@@ -192,9 +123,49 @@ int security_binder_transfer_binder(struct task_struct *from, struct task_struct
 	return security_binder_transfer_binder_ptr(from, to);
 }
 
+static int (*security_binder_transfer_file_ptr)(struct task_struct *from, struct task_struct *to, struct file *file) = NULL;
+
 int security_binder_transfer_file(struct task_struct *from, struct task_struct *to, struct file *file)
 {
 	if (!security_binder_transfer_file_ptr)
 		security_binder_transfer_file_ptr = kallsyms_lookup_name_wrapper("security_binder_transfer_file");
 	return security_binder_transfer_file_ptr(from, to, file);
+}
+
+static int (*task_work_add_ptr)(struct task_struct *task, struct callback_head *work,
+		  enum task_work_notify_mode notify) = NULL;
+
+int task_work_add(struct task_struct *task, struct callback_head *work,
+		  enum task_work_notify_mode notify)
+{
+	if (!task_work_add_ptr)
+		task_work_add_ptr = kallsyms_lookup_name_wrapper("task_work_add");
+	return task_work_add_ptr(task, work, notify);
+}
+
+static void (*zap_page_range_ptr)(struct vm_area_struct *, unsigned long, unsigned long) = NULL;
+
+void zap_page_range(struct vm_area_struct *vma, unsigned long address, unsigned long size)
+{
+	if (!zap_page_range_ptr)
+		zap_page_range_ptr = kallsyms_lookup_name_wrapper("zap_page_range");
+	zap_page_range_ptr(vma, address, size);
+}
+
+static void (*put_ipc_ns_ptr)(struct ipc_namespace *ns) = NULL;
+
+void put_ipc_ns(struct ipc_namespace *ns)
+{
+	if (!put_ipc_ns_ptr)
+		put_ipc_ns_ptr = kallsyms_lookup_name_wrapper("put_ipc_ns");
+	put_ipc_ns_ptr(ns);
+}
+
+static struct ipc_namespace *init_ipc_ns_ptr = NULL;
+
+struct ipc_namespace *get_init_ipc_ns_ptr(void)
+{
+	if (!init_ipc_ns_ptr)
+		init_ipc_ns_ptr = kallsyms_lookup_name_wrapper("init_ipc_ns");
+	return init_ipc_ns_ptr;
 }
